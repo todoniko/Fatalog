cd976e5b4b1250b0d169415808cbf43033fc4a92
todoniko <nmtodorov@gmail.com>
2022-06-22 14:40:54 +0300

    [Add] base types for datalog DSL

---
+++ lib/Datalog.fs
@@ -0,0 +1,22 @@
+﻿namespace Fatalog.DSL
+
+(*
+    P ∈ Program = c1,...,cn
+    c ∈ Clause  = l0 <- l1,...,ln
+    l ∈ Literal = p(t1,...,tn)
+    t ∈ Term    = k|x
+
+    Where
+    p = finite set of predicate symbols.
+    k = finite set of variable symbols.
+    x = finite set of constants.
+*)
+
+type Term =
+    | Var of string
+    | Sym of string
+
+type Predicate = string
+type Literal = Predicate * Term list
+type Clause = { Head: Literal; Body: Literal list }
+type Program = Clause list
